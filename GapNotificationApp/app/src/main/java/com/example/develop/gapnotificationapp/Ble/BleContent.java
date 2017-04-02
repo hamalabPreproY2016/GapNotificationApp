04fcc8c609b5573683c56a6aabf4a167a00a73e3
ragrog <ragrog216@outlook.jp>
2017-04-02 17:29:38 +0900

    BLEFragmentで使うadapterと、BLEの通信を行うclassを実装

---
+++ GapNotificationApp/app/src/main/java/com/example/develop/gapnotificationapp/Ble/BleContent.java
@@ -0,0 +1,115 @@
+package com.example.develop.gapnotificationapp.Ble;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.example.develop.gapnotificationapp.GapNotificationApplication;
+import com.example.develop.gapnotificationapp.R;
+import com.example.develop.gapnotificationapp.util.HexString;
+import com.polidea.rxandroidble.RxBleConnection;
+import com.polidea.rxandroidble.RxBleDevice;
+
+import java.util.UUID;
+
+import rx.Observable;
+import rx.subjects.PublishSubject;
+
+/**
+ * Created by ragro on 2017/03/30.
+ */
+
+abstract public class BleContent {
+    private String _mac_address;
+    private RxBleDevice _device;
+    private Context _context;
+    private UUID _writeUUID;
+    private UUID _notifUUID;
+    private byte[] _writeBytes;
+    public String TAG = "BLECONNTENT";
+    private PublishSubject<Void> disconnectTriggerSubject = PublishSubject.create();
+    private RxBleDevice _bleDevice;
+    private Observable<RxBleConnection> connectionObservable;
+
+    // 更新されたデータを読み取り
+    abstract public void Notification(byte[] readData);
+
+    public BleContent(Context context, String mac_address, UUID WriteUUID, UUID NotifiUUID) {
+        _mac_address = mac_address;
+        _context = context;
+        _writeUUID = WriteUUID;
+        _notifUUID = NotifiUUID;
+        _bleDevice = GapNotificationApplication.getRxBleClient(_context).getBleDevice(_mac_address);
+        connectionObservable = prepareConnectionObservable();
+        Log.d(TAG, "できてる");
+    }
+
+    private Observable<RxBleConnection> prepareConnectionObservable() {
+        return _bleDevice
+                .establishConnection(_context, false)
+                .takeUntil(disconnectTriggerSubject);
+    }
+    // 書込みデータ
+    public void Write(byte[] WriteData){
+        _writeBytes = WriteData;
+    }
+
+    // 接続
+    public void Connect(){
+        connectionObservable
+                .flatMap(rxBleConnection -> rxBleConnection.writeCharacteristic(_writeUUID, HexString.hexToBytes("00"))
+                        .flatMap(bytes ->rxBleConnection.setupNotification(_notifUUID))
+                        .doOnNext(notificationObservable -> {
+                            Log.d(TAG, "Notification has been set up");
+                            // Notification has been set up
+                        })
+                        .flatMap(notificationObservable -> notificationObservable)
+
+                )
+                .subscribe(
+                        bytes -> {
+                            Log.d(TAG, Integer.toString(bytes.length));
+                            Notification(bytes);
+                            // Written data.
+                        },
+                        throwable -> {
+                            // Handle an error here.
+                        }
+                );
+    }
+    // 接続解除
+    public void DisConnect(){
+        disconnectTriggerSubject.onNext(null);
+    }
+    // 接続中
+    public boolean Connected(){
+        return _bleDevice.getConnectionState() == RxBleConnection.RxBleConnectionState.CONNECTED;
+    }
+
+    public void Test(){
+
+        Log.d(TAG, _context.getResources().getString(R.string.uuid_notify));
+//        _device = BleConnectApplication.getRxBleClient(_context).getBleDevice(_mac_address);
+
+        connectionObservable
+                .flatMap(rxBleConnection -> rxBleConnection.writeCharacteristic(_writeUUID, HexString.hexToBytes("00"))
+                        .flatMap(bytes ->rxBleConnection.setupNotification(_notifUUID))
+                        .doOnNext(notificationObservable -> {
+                            Log.d(TAG, "Notification has been set up");
+                            // Notification has been set up
+                        })
+                                .flatMap(notificationObservable -> notificationObservable)
+
+                )
+                .subscribe(
+                        bytes -> {
+                            Log.d(TAG, HexString.bytesToHex(bytes));
+                            Write(bytes);
+                            // Written data.
+                        },
+                        throwable -> {
+                            // Handle an error here.
+                        }
+                );
+    }
+
+}
---
+++ GapNotificationApp/app/src/main/java/com/example/develop/gapnotificationapp/Ble/BleScanResultsAdapter.java
@@ -0,0 +1,168 @@
+package com.example.develop.gapnotificationapp.Ble;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.develop.gapnotificationapp.R;
+import com.example.develop.gapnotificationapp.util.BinaryInteger;
+import com.example.develop.gapnotificationapp.util.HexString;
+import com.polidea.rxandroidble.RxBleDevice;
+import com.polidea.rxandroidble.RxBleScanResult;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by ragro on 2017/03/28.
+ */
+
+public class BleScanResultsAdapter extends BaseAdapter {
+    private LayoutInflater _inflater;
+    private int _layoutID;
+    private List<BleViewItem> _bleDevicesList= new ArrayList<BleViewItem>();
+    private Ble_0 _bleContet;
+    private boolean flag = true;
+
+    static class ViewHolder {
+        TextView deviceName;
+        TextView macAddress;
+        TextView RSSI;
+        TextView readValue;
+        Button connectToggle;
+    }
+
+    public BleScanResultsAdapter(Context context){
+
+        _inflater = LayoutInflater.from(context);
+        _layoutID = R.layout.ble_scan_results_item;
+        _bleContet = new Ble_0(context,
+                context.getResources().getString(R.string.ble_0_mac_adress),
+                UUID.fromString(context.getResources().getString(R.string.uuid_write)),
+                UUID.fromString(context.getResources().getString(R.string.uuid_notify)));
+
+//context.getResources().getString(R.string.ble_0_mac_adress)
+    }
+    private class Ble_0 extends BleContent {
+
+        public Ble_0(Context context, String mac_address, UUID WriteUUID, UUID NotifiUUID) {
+            super(context, mac_address, WriteUUID, NotifiUUID);
+        }
+
+        @Override
+        public void Notification(byte[] readData){
+            Log.d(TAG, "おおい");
+            int num = BinaryInteger.TwoByteToInteger(readData);
+            Log.d(TAG, Integer.toString(num));
+        }
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        // 新規表示のとき
+        if (convertView == null) {
+            convertView = _inflater.inflate(_layoutID, null);
+            holder = new ViewHolder();
+            holder.deviceName = (TextView)convertView.findViewById(R.id.ble_device_item_device_name);
+            holder.macAddress = (TextView)convertView.findViewById(R.id.ble_device_item_mac_address);
+            holder.RSSI = (TextView)convertView.findViewById(R.id.ble_device_item_rssi);
+            holder.readValue = (TextView)convertView.findViewById(R.id.ble_device_item_read);
+            holder.connectToggle = (Button)convertView.findViewById(R.id.ble_device_item_toggle);
+            holder.connectToggle.setOnClickListener(new ConnectClickListener());
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        BleViewItem item = _bleDevicesList.get(position);
+        RxBleDevice device = item.device;
+        holder.deviceName.setText(device.getName());
+        holder.macAddress.setText(device.getMacAddress());
+        holder.RSSI.setText("RSSI : " + Integer.toString(item.rssi));
+        holder.readValue.setText("Read : " + item.read);
+        holder.connectToggle.setTag(device.getMacAddress());
+        _bleDevicesList.get(position).holder = holder;
+
+        return convertView;
+    }
+
+    public void addScanResult(RxBleScanResult bleScanResult) {
+            // Not the best way to ensure distinct devices, just for sake on the demo.
+
+            for (int i = 0; i < _bleDevicesList.size(); i++) {
+
+                if (_bleDevicesList.get(i).device.equals(bleScanResult.getBleDevice())) {
+                    _bleDevicesList.get(i).rssi = bleScanResult.getRssi();
+                    _bleDevicesList.get(i).read = "";
+                    _bleDevicesList.get(i).holder.RSSI.setText("RSSI : " + Integer.toString(bleScanResult.getRssi()));
+                    return;
+                }
+            }
+
+            _bleDevicesList.add(new BleViewItem(bleScanResult.getBleDevice(), bleScanResult.getRssi(), ""));
+            Collections.sort(_bleDevicesList, SORTING_COMPARATOR);
+            notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+        return _bleDevicesList.size();
+
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return _bleDevicesList.get(position);
+    }
+
+    public void clearScanResults() {
+        _bleDevicesList.clear();
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public class BleViewItem {
+        public RxBleDevice device;
+        public String read;
+        public int rssi;
+        public ViewHolder holder;
+        public BleViewItem(RxBleDevice _device, int _rssi, String _read) {
+            device = _device;
+            read = _read;
+            rssi = _rssi;
+        }
+    }
+    // ソート用のラムダ式
+    private static final Comparator<BleViewItem> SORTING_COMPARATOR = (lhs, rhs) -> {
+        return lhs.device.getName().compareTo(rhs.device.getName());
+    };
+    public class ConnectClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            String address =(String)view.getTag();
+            if(_bleContet.Connected()){
+                ((Button)view).setText("CONNECT");
+                _bleContet.DisConnect();
+            }else {
+                Log.d(_bleContet.TAG, "こっち?");
+                ((Button) view).setText("DISCONNECT");
+                _bleContet.Connect();
+            }
+        }
+    }
+
+
+}
+
