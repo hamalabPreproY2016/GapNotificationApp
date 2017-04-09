36b2e72728925f6e4fc69a1ae1c51dc10370011f
ragrog <ragrog216@outlook.jp>
2017-04-09 13:16:02 +0900

    実験ログを操作する昨日を実装

---
+++ GapNotificationApp/app/src/main/java/com/example/develop/gapnotificationapp/Log/GapFileManager.java
@@ -0,0 +1,79 @@
+package com.example.develop.gapnotificationapp.Log;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by ragro on 2017/04/08.
+ */
+
+public class GapFileManager {
+    private String _root = "DataLog";
+    private Context _context;
+    private File _rootDirectory;
+    private SimpleDateFormat format;
+    public GapFileManager(Context context){
+        _context = context;
+        _root = _context.getFilesDir() +"/" +  _root;
+        _rootDirectory = new File(_root);
+        Log.d("GapFileManger",_root);
+        if(!_rootDirectory.exists() || !_rootDirectory.isDirectory()) {
+            _rootDirectory.mkdir();
+            Log.d("GapFileManger", "create new root directory");
+        }
+        format = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+    }
+    // 新しいLogディレクトリを現在の日時で作成
+    public File getNewLogDirectory(){
+        File[] files = _rootDirectory.listFiles();
+        Log.d("GapFileManger", _root + "/" +Integer.toString(files.length));
+        File result = new File(_rootDirectory, format.format(new Date()));
+        result.mkdir();
+        return result;
+    }
+    // 全てのLogDirectoryを削除する
+    public void clearAllLogDirectory(){
+        Log.d("GapFileManger", Integer.toString(_rootDirectory.list().length));
+        if (_rootDirectory.isDirectory())
+        {
+            String[] children = _rootDirectory.list();
+            for (int i = 0; i < children.length; i++)
+            {
+                new File(_rootDirectory, children[i]).delete();
+            }
+        }
+    }
+    // 指定の日付に対応するLogディレクトリを削除
+    public  void deleteLogDirectory(String string){
+        File delete = new File(_rootDirectory,string);
+        delete.delete();
+        // もしかしたら、ファイルが中に入っていたら消せないかも
+    }
+    // Logディレクトリの数を取得
+    public int getLogDirectoryNum(){
+        return _rootDirectory.list().length;
+    }
+    // IDに対応するLogディレクトリを取得 無ければnullを返す
+    public File getLogDirectory(int ID){
+
+        File result = new File(_rootDirectory, Integer.toString(ID));
+        if(!_rootDirectory.exists() || !_rootDirectory.isDirectory()) {
+            return result;
+        } else {
+            return null;
+        }
+    }
+
+    // 現在のLogディレクトリを全て取得
+    public File[] getLogDirectories(){
+        if (_rootDirectory.isDirectory())
+        {
+           return  _rootDirectory.listFiles();
+        }
+        return null;
+    }
+}
--- GapNotificationApp/app/src/main/java/com/example/develop/gapnotificationapp/LogListFragment.java
+++ GapNotificationApp/app/src/main/java/com/example/develop/gapnotificationapp/LogListFragment.java
@@ -6,6 +6,13 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.example.develop.gapnotificationapp.Log.GapFileManager;
+import com.example.develop.gapnotificationapp.Log.LogDirectoryListAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
@@ -13,17 +20,26 @@ import android.view.ViewGroup;
  */
 public class LogListFragment extends Fragment {
 
-
+    @BindView(R.id.log_directory_list)
+    public ListView list;
     public LogListFragment() {
         // Required empty public constructor
+
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_log_list, container, false);
+        ButterKnife.bind(this, view);
+        
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_list, container, false);
+        GapFileManager gfm = new GapFileManager(getActivity());
+        gfm.getNewLogDirectory();
+        LogDirectoryListAdapter adapter = new LogDirectoryListAdapter(getActivity(), gfm);
+        list.setAdapter(adapter);
+        return view;
     }
 
 }
