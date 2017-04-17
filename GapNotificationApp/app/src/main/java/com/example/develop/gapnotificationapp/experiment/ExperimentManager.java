8a12d41bd5698b123489a6728ca93005edf0175b
ragrog <ragrog216@outlook.jp>
2017-04-17 19:21:21 +0900

    実験管理クラスとそのリスナーをmodelクラスを使用するようにした

--- GapNotificationApp/app/src/main/java/com/example/develop/gapnotificationapp/experiment/ExperimentManager.java
+++ GapNotificationApp/app/src/main/java/com/example/develop/gapnotificationapp/experiment/ExperimentManager.java
@@ -4,7 +4,12 @@ import android.content.Context;
 import android.util.Log;
 
 import com.example.develop.gapnotificationapp.Log.GapFileManager;
+import com.example.develop.gapnotificationapp.model.Emg;
+import com.example.develop.gapnotificationapp.model.Face;
+import com.example.develop.gapnotificationapp.model.Heartrate;
+import com.example.develop.gapnotificationapp.model.Voice;
 import com.example.develop.gapnotificationapp.rest.Pojo.EmgAverage.request.RequestAverage;
+import com.example.develop.gapnotificationapp.rest.Pojo.EmgAverage.response.ResponseAverage;
 import com.example.develop.gapnotificationapp.rest.RestManager;
 import com.example.develop.gapnotificationapp.voice.RealTimeVoiceSlicer;
 import com.example.develop.gapnotificationapp.voice.VoiceSliceListener;
@@ -13,6 +18,11 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.LongSummaryStatistics;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by ragro on 2017/04/12.
@@ -32,11 +42,12 @@ public class ExperimentManager {
     // 筋電平均取得時間関係
     private final long _average_time_milliseconds = 60 * 5 * 1000; // 5分間の平均取得時間
     private boolean _isGetAverageEmgSession;
+    private int _averageEmg;
 
-    private List<SensorStruct.VoiceStruct> _voiceData = new ArrayList<SensorStruct.VoiceStruct>(); // 音声データ
-    private List<SensorStruct.FaceStruct> _faceData = new ArrayList<SensorStruct.FaceStruct>(); // カメラデータ
-    private List<SensorStruct.HeartRateStruct> _heartRateData = new ArrayList<SensorStruct.HeartRateStruct>(); // 心拍データ
-    private List<SensorStruct.EmgStruct> _emgData = new ArrayList<SensorStruct.EmgStruct>(); // 筋電データ
+    private List<Voice> _voiceData = new ArrayList<>(); // 音声データ
+    private List<Face> _faceData = new ArrayList<>(); // カメラデータ
+    private List<Heartrate> _heartRateData = new ArrayList<>(); // 心拍データ
+    private List<Emg> _emgData = new ArrayList<>(); // 筋電データ
 
     private int[] _cacheStartIndexList = new int[4]; // 各センサーのキャッシュIndex
 
@@ -114,50 +125,59 @@ public class ExperimentManager {
 
     // 音声データを一時的に記憶
     private void setVoiceCache(File data){
+        Voice voice = new Voice();
+        voice.file = data;
+        voice.time = Long.toString(getRemmaningTime());
         long current_time = getRemmaningTime();
         // 音声データリストにデータを追加
-        _voiceData.add(_sensor.new VoiceStruct(data, current_time));
+        _voiceData.add(voice);
         _flag[Device.VOICE.ordinal()] = true;
         // リスナーを呼び出し
         if (_listener != null) {
-            _listener.GetVoice(_sensor.new VoiceStruct(data, current_time));
+            _listener.GetVoice(voice);
         }
         sendApiServer();
     }
 
     // 写真データを一時的に記憶
     private void setCameraCache(File data){
-        long current_time = getRemmaningTime();
+        Face face = new Face();
+        face.file = data;
+        face.time = Long.toString(getRemmaningTime());
         // 写真データリストにデータを追加
-        _faceData.add(_sensor.new FaceStruct(data, current_time));
+        _faceData.add(face);
         _flag[Device.FACE.ordinal()] = true;
         // リスナーを呼び出し
         if (_listener != null) {
-            _listener.GetFace(_sensor.new FaceStruct(data, current_time));
+            _listener.GetFace(face);
         }
         sendApiServer();
     }
 
     // 心拍データを一時的に記憶
     private void setHeartRateCache(Short data){
-        long current_time = getRemmaningTime();
+        Heartrate heart = new Heartrate();
+        heart.time = Long.toString(getRemmaningTime());
+        heart.value = data.intValue();
         // 心拍データリストにデータを追加
-        _heartRateData.add(_sensor.new HeartRateStruct(data, current_time));
+        _heartRateData.add(heart);
         _flag[Device.HEARTRATE.ordinal()] = true;
         if (_listener != null) {
-            _listener.GetHeartRate(_sensor.new HeartRateStruct(data, current_time));
+            _listener.GetHeartRate(heart);
         }
         sendApiServer();
     }
 
     // 筋電データを一時的に記憶
     private void setEmgCache(Short data) {
-        long current_time = getRemmaningTime();
+        Emg emg = new Emg();
+        emg.time = Long.toString(getRemmaningTime());
+        emg.value = data.intValue();
         // 筋電データリストにデータを追加
-        _emgData.add(_sensor.new EmgStruct(data, current_time));
+        _emgData.add(emg);
         _flag[Device.EMG.ordinal()] = true;
         if (_listener != null) {
-            _listener.GetEmg(_sensor.new EmgStruct(data, current_time));
+            _listener.GetEmg(emg);
         }
         sendApiServer();
     }
@@ -189,15 +209,15 @@ public class ExperimentManager {
         // 全てのデータがキャッシュされていない場合は送らない
         if (!isAllCompleted()) return;
 
-        // 各センサーのキャッシュを取り出す
-        List<SensorStruct.VoiceStruct> voice_cache =
-                new ArrayList<>(_voiceData.subList(_cacheStartIndexList[Device.VOICE.ordinal()], _voiceData.size() )); // 音声データ
-        List<SensorStruct.FaceStruct> face_cache =
-                new ArrayList<>(_faceData.subList(_cacheStartIndexList[Device.FACE.ordinal()], _faceData.size() )); // 顔写真データ
-        List<SensorStruct.EmgStruct> emg_cache =
-                new ArrayList<>(_emgData.subList(_cacheStartIndexList[Device.EMG.ordinal()], _emgData.size() )); // 筋電データ
-        List<SensorStruct.HeartRateStruct> heartrate_cache =
-                new ArrayList<>(_heartRateData.subList(_cacheStartIndexList[Device.HEARTRATE.ordinal()] ,_heartRateData.size() )); // 心拍データ
+//        // 各センサーのキャッシュを取り出す
+//        List<SensorStruct.VoiceStruct> voice_cache =
+//                new ArrayList<>(_voiceData.subList(_cacheStartIndexList[Device.VOICE.ordinal()], _voiceData.size() )); // 音声データ
+//        List<SensorStruct.FaceStruct> face_cache =
+//                new ArrayList<>(_faceData.subList(_cacheStartIndexList[Device.FACE.ordinal()], _faceData.size() )); // 顔写真データ
+//        List<SensorStruct.EmgStruct> emg_cache =
+//                new ArrayList<>(_emgData.subList(_cacheStartIndexList[Device.EMG.ordinal()], _emgData.size() )); // 筋電データ
+//        List<SensorStruct.HeartRateStruct> heartrate_cache =
+//                new ArrayList<>(_heartRateData.subList(_cacheStartIndexList[Device.HEARTRATE.ordinal()] ,_heartRateData.size() )); // 心拍データ
 
         // Requestデータを作る
 
@@ -214,10 +234,23 @@ public class ExperimentManager {
     private void SessionAverageEMG(){
         // 現在の経過時間が平常値取得時間よりも長かった場合は
         if (getRemmaningTime() > _average_time_milliseconds){
+            // pojofileを作成
             RequestAverage request_average = new RequestAverage();
-            request_average.emg = new ArrayList<>();
+            request_average.emg = new ArrayList<>(_emgData);
+            // 筋電をAPIに送って平均値を取得する
+            _restManager.postEmgAverage(request_average, new Callback<ResponseAverage>() {
+                @Override
+                public void onResponse(Call<ResponseAverage> call, Response<ResponseAverage> response) {
+                    Log.d(TAG, "average emg :" + Double.toString(response.body().average));
+                    _isGetAverageEmgSession = false;
+                    _averageEmg = response.body().average;
+                }
 
-//_restManager.postEmgAverage();
+                @Override
+                public void onFailure(Call<ResponseAverage> call, Throwable t) {
+
+                }
+            });
         }
     }
 
--- GapNotificationApp/app/src/main/java/com/example/develop/gapnotificationapp/experiment/ExperimentManagerListener.java
+++ GapNotificationApp/app/src/main/java/com/example/develop/gapnotificationapp/experiment/ExperimentManagerListener.java
@@ -1,12 +1,17 @@
 package com.example.develop.gapnotificationapp.experiment;
 
+import com.example.develop.gapnotificationapp.model.Emg;
+import com.example.develop.gapnotificationapp.model.Face;
+import com.example.develop.gapnotificationapp.model.Heartrate;
+import com.example.develop.gapnotificationapp.model.Voice;
+
 /**
  * Created by ragro on 2017/04/16.
  */
 
 public abstract class ExperimentManagerListener {
-    public abstract void GetHeartRate(SensorStruct.HeartRateStruct data);
-    public abstract void GetEmg(SensorStruct.EmgStruct data);
-    public abstract void GetVoice(SensorStruct.VoiceStruct data);
-    public abstract void GetFace(SensorStruct.FaceStruct data);
+    public abstract void GetHeartRate(Heartrate data);
+    public abstract void GetEmg(Emg data);
+    public abstract void GetVoice(Voice data);
+    public abstract void GetFace(Face data);
 }
