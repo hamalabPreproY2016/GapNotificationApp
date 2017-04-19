d174cfee0584030c3f271771e9895edc8d735cfb
ragrog <ragrog216@outlook.jp>
2017-04-19 21:33:49 +0900

    実験開始から終了までの一連の流れの結合とリスナーを作成

--- GapNotificationApp/app/src/main/java/com/example/develop/gapnotificationapp/experiment/ExperimentManagerListener.java
+++ GapNotificationApp/app/src/main/java/com/example/develop/gapnotificationapp/experiment/ExperimentManagerListener.java
@@ -3,6 +3,7 @@ package com.example.develop.gapnotificationapp.experiment;
 import com.example.develop.gapnotificationapp.model.Emg;
 import com.example.develop.gapnotificationapp.model.Face;
 import com.example.develop.gapnotificationapp.model.Heartrate;
+import com.example.develop.gapnotificationapp.model.ResponseAngry;
 import com.example.develop.gapnotificationapp.model.Voice;
 
 /**
@@ -10,8 +11,10 @@ import com.example.develop.gapnotificationapp.model.Voice;
  */
 
 public abstract class ExperimentManagerListener {
-    public abstract void GetHeartRate(Heartrate data);
-    public abstract void GetEmg(Emg data);
-    public abstract void GetVoice(Voice data);
-    public abstract void GetFace(Face data);
+    public abstract void GetHeartRate(Heartrate data); // 心拍が取得できた時に呼び出される
+    public abstract void GetEmg(Emg data);  // 筋電が取得できた時に呼び出される
+    public abstract void GetVoice(Voice data); // 音声が取得できた時に呼び出される
+    public abstract void GetFace(Face data); // 写真が取得できた時に呼び出される
+    public abstract void GetEmgAverage(int average); // 筋電の平均値がAPIサーバから取得できた時に呼び出される
+    public abstract void GetAngry(ResponseAngry response); // 怒り判定の結果がAPIサーバから取得できた時に呼び出される
 }
